31376e76e04dfd8fc616b16f24ba01981db216cc
waratip1801-sys <waratip.1801@gmail.com>
2026-04-03 23:03:09 +0700

    fix db connection

--- config.php
+++ config.php
@@ -1,17 +1,13 @@
 <?php
-// ตั้งค่าการเชื่อมต่อฐานข้อมูล
-$host = "localhost";
+$host = "junction.proxy.rlwy.net";
+$port = 37604;
 $user = "root";
-$pass = "";
-$db   = "thaphet_website";
+$pass = "LtvqydRohNxyXdZZQhtZPqEAgfiZuvsy";
+$db   = "railway";
 
-// เชื่อมต่อฐานข้อมูล
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-// ตรวจสอบการเชื่อมต่อ
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-// ป้องกันภาษาไทยกลายเป็น ????
 mysqli_set_charset($conn, "utf8");
+?>
