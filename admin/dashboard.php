903d8e84b5d32f9be5aacfa4dc6fb0ae43272e0a
fuengfah01 <yasuriya_w@silpakorn.edu>
2026-04-04 06:00:36 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,7 @@ include 'check_login.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="/thaphet_website/assets/css/admin.css">
+    <link rel="stylesheet" href="/assets/css/admin.css">
 </head>
 <body>
 
