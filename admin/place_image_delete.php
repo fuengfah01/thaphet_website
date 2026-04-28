4a2e00e987b68ffcc9bcd618e04ba3291b979c7b
fuengfah01 <yasuriya_w@silpakorn.edu>
2026-04-28 22:39:41 +0700

    Update place_image_delete.php

--- admin/place_image_delete.php
+++ admin/place_image_delete.php
@@ -2,11 +2,12 @@
 include '../config.php';
 include 'check_login.php';
 
-$image_id = $_GET['id'];
-$place_id = $_GET['place_id'];
+$image_id = (int)($_GET['id'] ?? 0);
+$place_id = (int)($_GET['place_id'] ?? 0);
 
-// ลบจาก DB อย่างเดียว (รูปอยู่บน Cloudinary)
-mysqli_query($conn, "DELETE FROM place_image WHERE image_id = $image_id");
+if ($image_id && $place_id) {
+    mysqli_query($conn, "DELETE FROM place_image WHERE image_id = $image_id");
+}
 
 header("Location: place_edit.php?id=$place_id");
 exit;
