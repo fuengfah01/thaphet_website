50e1c14f3b11f8fe01d23bf225b8d049e41b813a
fuengfah01 <yasuriya_w@silpakorn.edu>
2026-04-28 22:37:51 +0700

    Update place_delete.php

--- admin/place_delete.php
+++ admin/place_delete.php
@@ -2,24 +2,16 @@
 include '../config.php';
 include 'check_login.php';
 
-$id = $_GET['id'] ?? 0;
+$id = (int)($_GET['id'] ?? 0);
 
 if ($id) {
-
-    // 1) ดึง path รูป เพื่อจะลบไฟล์จริง
-    $res = mysqli_query($conn, "SELECT image_path FROM place_image WHERE place_id = $id");
-
-    while ($row = mysqli_fetch_assoc($res)) {
-        $image_path = "assets/image/" . $filename;
-        if (file_exists($file)) {
-            unlink($file); // ลบไฟล์ออกจาก server
-        }
-    }
-
-    // 2) ลบรูปจาก DB ก่อน
+    // ลบรูปจาก DB ก่อน
     mysqli_query($conn, "DELETE FROM place_image WHERE place_id = $id");
-
-    // 3) แล้วค่อยลบ place
+    
+    // ลบ model_3d ที่ผูกกับ place นี้
+    mysqli_query($conn, "DELETE FROM model_3d WHERE place_id = $id");
+    
+    // แล้วค่อยลบ place
     mysqli_query($conn, "DELETE FROM place WHERE place_id = $id");
 }
 
